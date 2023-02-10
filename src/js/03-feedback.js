f9f9f644ba7d24d929fd3d96899564e5bb6cb746
Vladyslav Simak <simakv21@gmail.com>
2023-02-10 12:44:19 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
+
+const formValue = document.querySelector('form');
+const buttonSubmit = document.querySelector('button');
+getValueLocalStorage();
+
+const formDataSet = {};
+
+formValue.addEventListener('input', throttle(getValueTextArea, 500));
+
+function getValueTextArea(event) {
+  formDataSet[event.target.name] = event.target.value;
+  localStorage.setItem("feedback-form-state", JSON.stringify(formDataSet));
+}
+
+buttonSubmit.addEventListener('click', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log('Відправляємо форму');
+
+  event.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+};
+
+function getValueLocalStorage() {
+  const saveMessage = localStorage.getItem('feedback-form-state');
+  const formDataGet = JSON.parse(saveMessage);
+
+  try {
+    const formDataGet = JSON.parse(saveMessage);
+    formValue.email.value = formDataGet.email;
+    formValue.message.value = formDataGet.message;
+  } catch (error) {
+    console.log(error.name); // "SyntaxError"
+    console.log(error.message); // "Unexpected token u in JSON at position 1"
+  }
+  
+  console.log(formDataGet);
+};
 

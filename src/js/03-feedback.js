0a597c2ab4346623b0ce0a7ee88e9bace47e8fdb
Vladyslav Simak <simakv21@gmail.com>
2023-02-10 15:17:51 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,6 +5,7 @@ const buttonSubmit = document.querySelector('button');
 
 const formDataSet = {};
 
+
 formValue.addEventListener('input', throttle(getValueTextArea, 500));
 
 function getValueTextArea(event) {
@@ -12,28 +13,27 @@ function getValueTextArea(event) {
   localStorage.setItem("feedback-form-state", JSON.stringify(formDataSet));
 }
 
+const dataBackEnd = localStorage.getItem('feedback-form-state');
+
 buttonSubmit.addEventListener('click', onFormSubmit);
 
 function onFormSubmit() {
-  console.log(formDataSet);
-  formValue.email.value = '';
-  formValue.message.value = '';
+  console.log(dataBackEnd); 
   localStorage.removeItem('feedback-form-state');
-
+  formValue.reset();
 };
 
 function getValueLocalStorage() {
-  const saveMessage = localStorage.getItem('feedback-form-state');
 
   try {
-    const formDataGet = JSON.parse(saveMessage);
+    const formDataGet = JSON.parse(dataBackEnd);
     formValue.email.value = formDataGet.email;
     formValue.message.value = formDataGet.message;
   } catch (error) {
     console.log(error.name); 
     console.log(error.message); 
   }
-  
+
 };
 
 getValueLocalStorage();

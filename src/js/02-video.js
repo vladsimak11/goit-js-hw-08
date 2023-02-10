3a89996fe1937cda380291977d59bd57fac99382
Vladyslav Simak <simakv21@gmail.com>
2023-02-10 11:04:18 +0200

    Task №2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
+  
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+// Add time to localStorage
+
+player.on('timeupdate', throttle((data) => {
+  localStorage.setItem("videoplayer-current-time", data.seconds);
+}, 1000)
+);
+
+// Get time from localStorage
+
+const getCurrentTime = localStorage.getItem("videoplayer-current-time");
+
+player.setCurrentTime(getCurrentTime).then(function(seconds) {
+  console.log(seconds);
+}).catch(function(error) {
+  switch (error.name) {
+    case 'RangeError':
+      break;
+
+    default:
+      break;
+  }
+});
+
 
